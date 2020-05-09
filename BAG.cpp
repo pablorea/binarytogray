002c880b7e33fcabaf3bb78ffc7eabc205e40b14
Pablo Leandro Rea <pablorea.dev@gmail.com>
2020-05-09 14:54:46 -0300

    Binary to gray

---
+++ BAG.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main (){
+	
+    long long  binario, a, b, i = 0, gray =0;
+    
+    cout << "Numero binario: ";
+    cin >> binario;
+    
+    while (binario != 0)
+    {
+        a = binario % 10;      
+        binario = binario / 10; 
+        b = binario % 10;      
+        
+        if ((a && !b) || (!a && b)) 
+            gray = gray + pow(10, i);
+        i++;
+    }
+
+    cout << "Codigo gray: "<< gray;
+    return 0;
+}
